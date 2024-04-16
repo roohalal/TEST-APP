564cb48008a04226e59dba6381b1905b60fd7ec6
Shazeb Suhail <shazebs7@outlook.com>
2024-04-16 11:36:20 -0700

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>Software Job Applications Tracker</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
